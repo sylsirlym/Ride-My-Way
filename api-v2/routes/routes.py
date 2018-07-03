335246048c07fb0ef0f6ba5d95e12d2b3ca764ba
Sylvester Musyoki <musyokisyl81@gmail.com>
2018-07-03 22:30:19 +0300

    Update routes.py

--- api-v2/routes/routes.py
+++ api-v2/routes/routes.py
@@ -1,15 +1,17 @@
 import os
+from os import environ
 from flask import Flask,jsonify,request, make_response, abort
 import json
 import psycopg2
 
 app = Flask (__name__)
+app.config['CONN_STRING'] = environ.get('DB_CONN_STRING')
+
 
 def connectDB():
     
     #Define a connection string
-    conn_string = "host='localhost' dbname='ridemyway' user='postgres' password='Secrets'"
-    
+    conn_string = app.config['CONN_STRING']
     #Initialize a connection, if a connect cannot be made an exception will be raised here
     conn = psycopg2.connect(conn_string)
  
@@ -21,4 +23,4 @@ def connectDB():
 
 if __name__ == '__main__':
     connectDB()
-    # app.run(debug=True)
+    app.run(debug=True)
