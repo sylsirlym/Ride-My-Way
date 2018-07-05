6894ae4f721114260747c64361812f7dd094ebe8
sylsirlym <musyokisyl81@gmail.com>
2018-07-05 13:30:18 +0300

    Creat API endpoints with DB
    Change DB config
    Create a get all rides endpoint
    Create a get a single ride endpoint
    Create request endpoint
    Get all requests endpoint
    Resonsd to a request endpoint

--- api-v2/models/models.py
+++ api-v2/models/models.py
@@ -1,22 +1,13 @@
 import os
 import psycopg2
 import config
+from flask import jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class Database:
-    def __init__(self, app=None):
-        dbe= config.Config.db
-        self.conn=psycopg2.connect(dbname=dbe["DATABASE_NAME"], user=dbe["DATABASE_USER"],password=dbe["DATABASE_PASS"],host=dbe["DATABASE_HOST"])        
-        self.cur = self.conn.cursor()
-
-    def commit(self):
-        self.conn.commit()
-
-    def cursor(self):
-        return self.cur
-    
-
-database = Database()
+def dbconn():
+    dbe= config.Config.db
+    conn=psycopg2.connect(dbname=dbe["DATABASE_NAME"], user=dbe["DATABASE_USER"],password=dbe["DATABASE_PASS"],host=dbe["DATABASE_HOST"])
+    return conn    
 
 class User():
 
@@ -27,11 +18,13 @@ class User():
         self.email = email
         self.password = password
     
-    #def create_user(self):
-        cursor = database.cursor()
-        cursor.execute("INSERT INTO users (fname, lname, email, password) VALUES (%s, %s, %s, %s)",
+    def create_user(self):
+        conn = dbconn()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (fname, lname, email, password) VALUES (%s, %s, %s, %s)",
                                     (self.fname, self.lname, self.email, self.password))
-        database.commit()
+        conn.commit()
+        conn.close()
 
 class Ride():
     def __init__(self, user_id=None, start_loc=None, end_loc=None,departure_time=None, date=None, route=None, cost=None):
@@ -42,18 +35,93 @@ class Ride():
         self.date = date
         self.route = route
         self.cost = cost
-    # def create_ride()
-        cursor = database.cursor()
-        cursor.execute("INSERT INTO rides (user_id, start_loc, end_loc, depature_time, date, route, cost) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+    def create_ride(self):
+        conn = dbconn()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO rides (user_id, start_loc, end_loc, depature_time, date, route, cost) VALUES (%s, %s, %s, %s, %s, %s, %s)",
                                     (self.user_id, self.start_loc, self.end_loc, self.departure_time, self.date,self.route,self.cost))
-        database.commit()
+        conn.commit()
+        conn.close()
+    
+    @staticmethod
+    def get_rides():
+        conn = dbconn()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM rides;")
+        rows = cur.fetchall()
+
+        rides = {}
+        for row in rows:
+            rides[row[0]] = {
+                'user_id': row[1],
+                'start_loc':row[2],
+                'end_loc': row[3],
+                'departure_time': row[4],
+                'date': row[5],
+                'route': row[6],
+                'cost': row[7]
+            }
+        return rides
+    
+    @staticmethod
+    def get_ride(ride_id):
+        conn = dbconn()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM rides WHERE id=%s", (ride_id,))
+        row = cur.fetchone()
+
+        # if row is None:
+        #     return jsonify({
+        #         "message" : "Ride not found"}), 404
+
+        rides = {}
+        rides[row[0]] = {
+            'user_id': row[1],
+            'start_loc':row[2],
+            'end_loc': row[3],
+            'departure_time': row[4],
+            'date': row[5],
+            'route': row[6],
+            'cost': row[7]
+        }
+
+        return rides
+
 
 class Request():
         def __init__(self, ride_id =None,pickup_loc=None):
             self.ride_id = ride_id
             self.pickup_loc = pickup_loc
         def created_req(self):
-            cursor = database.cursor()
-            cursor.execute("INSERT INTO requests (ride_id, pickup_loc) VALUES (%s, %s)",
-                                        (self.ride_id, self.pickup_loc))
-            database.commit()
+            conn = dbconn()
+            cur = conn.cursor()
+            cur.execute("INSERT INTO requests (ride_id, pickup_loc) VALUES (%s, %s)",
+                                        [self.ride_id, self.pickup_loc])
+            conn.commit()
+            conn.close()
+    
+        @staticmethod
+        def get_requests():
+            conn = dbconn()
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM requests;")
+            rows = cur.fetchall()
+
+            req = {}
+            for row in rows:
+                req[row[0]] = {
+                    'ride_id': row[1],
+                    'status':row[2],
+                    'pickup_loc': row[3]
+                }
+            return req
+
+        @staticmethod
+        def requests_resp(respo, req_id):
+            conn = dbconn()
+            cur = conn.cursor()
+            cur.execute("UPDATE requests SET status=%s WHERE id=%s RETURNING status",(respo, req_id))
+            status = cur.fetchone()[0]
+            conn.commit()
+            conn.close()
+            return status
--- api-v2/models/routes.py
+++ api-v2/models/routes.py
@@ -17,7 +17,8 @@ def create_user():
     cpass = data.get('cpass')
     if fname is not None and lname is not None and email is not None and password is not None and cpass is not None: #Check deatils
         if cpass == password:
-            User(fname=fname, lname=lname, email=email, password=password)
+            new_user=User(fname=fname, lname=lname, email=email, password=password)
+            new_user.create_user()
             return jsonify({
                     "message" : "Welcome to Ride-My-Way"}), 201
         else:
@@ -27,6 +28,7 @@ def create_user():
         return jsonify({
                 "message" : "Please make sure all fileds are filled"}), 201
 
+#Rides
 @app.route('/api/v1/rides',  methods = ['POST'])
 def create_ride():
     data = request.get_json()
@@ -40,7 +42,7 @@ def create_ride():
 
     if user_id is not None and start_loc is not None and end_loc is not None and departure_time is not None and date is not None and route is not None and cost is not None:
 
-        Ride( 
+        new_ride= Ride( 
             user_id = user_id, 
             start_loc = start_loc , 
             end_loc = end_loc,  
@@ -49,6 +51,7 @@ def create_ride():
             route = route,
             cost = cost  
             )
+        new_ride.create_ride()
         return jsonify({
                 "message" : "Succesfully created"}), 201
 
@@ -56,23 +59,53 @@ def create_ride():
         return jsonify({
                 "message" : "Please fill in all the fields"}), 201
 
-@app.route('/api/v1/rides/<int:id>/requests',  methods = ['POST'])
-def create_request(id):
+@app.route('/api/v1/rides', methods = ['GET'])
+def get_rides():
+
+    rides = Ride.get_rides()
+    return jsonify({'rides': rides})
+
+@app.route('/api/v1/rides/<int:ride_id>', methods = ['GET'])
+def get_ride(ride_id):
+
+    rides = Ride.get_ride(ride_id)
+    return jsonify({'rides': rides})
+
+#Request
+@app.route('/api/v1/rides/<int:ride_id>/requests',  methods = ['POST'])
+def create_request(ride_id):
     data = request.get_json()
     ride_id = data.get('ride_id'),
     pickup_loc = data.get('pickup_loc'),
     
     
-    if ride_id is not None and pickup_loc is not None and status is not None:
+    if ride_id is not None and pickup_loc is not None:
 
-        Request(ride_id=ride_id, pickup_loc= pickup_loc)
-
+        new_req = Request(ride_id=ride_id, pickup_loc= pickup_loc)
+        new_req.created_req()
         return (jsonify({
                 "message" : "Request successfully submitted"}), 201)
     else:
         return jsonify({
                 "message" : "Please fill in all the fields"}), 201  
 
+@app.route('/api/v1/rides/<int:ride_id>/requests', methods = ['GET'])
+def get_requests(ride_id):
+
+    requests = Request.get_requests()
+    return jsonify({'requests': requests})
+
+@app.route('/api/v1/rides/<int:ride_id>/requests/<int:req_id>', methods = ['PUT'])
+def request_respo(ride_id, req_id):
+    
+    data = request.get_json()
+    respo = data.get('respo'),
+
+
+    if respo is not None:
+        req_resp = Request()
+        status = req_resp.requests_resp(respo, req_id)
+    return jsonify({'msg': "Request has been " + status})
 
 @app.route('/')
 def hello_world():
